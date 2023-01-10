3afe6b72ac4f8ddfbdccccf22acc6fda3517d6b2
akhileshu <umapati4381@gmail.com>
2023-01-10 10:07:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import { BrowserRouter,Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+// import Songui from "./components/Songui";
+// import audiofile from 'src/assets/2.mp3'
 
 function App() {
+  let song1 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3";
+  let song2 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3";
+  let song3 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3";
+  let song4 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3";
+  let song5 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3";
+  let song6 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3";
+  let song7 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3";
+  let song8 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3";
+  let song9 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3";
+  let song10 = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3";
+  let songs = [
+    { filepath: song1 },
+    { filepath: song2 },
+    { filepath: song3 },
+    { filepath: song4 },
+    { filepath: song5 },
+    { filepath: song6 },
+    { filepath: song7 },
+    { filepath: song8 },
+    { filepath: song9 },
+    { filepath: song10 },
+  ];
+  let songlist = songs.map((e, i) => {
+    return (
+      
+      <div key={i}>
+      <h2>song{i+1}</h2>
+      
+        <audio controls src={e.filepath}></audio>
+      </div>
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+   
+      <Navbar />
+    
+<div className="App">
+
+      <div>
+     <h1> try our best songs</h1>
+      {songlist}
+      </div>
     </div>
+  
+    </>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import './App.css'
+
+export default function () {
+  return (
+    <>
+<ul>
+    <li className='name'>Hi !{" Akhilesh"}</li>
+    
+    <li><a target='_blank' href="https://www.jiosaavn.com/"><img src="https://img.icons8.com/ios/2x/jiosaavn.png" alt="" /></a></li>
+    <li><a target='_blank' href="https://wynk.in/music"><img src="https://img.icons8.com/color-glass/2x/wynk-music.png" alt="" /></a></li>
+    <li><a target='_blank' href="https://gaana.com/"><img src="https://img.icons8.com/clouds/2x/gaana.png" alt="" /></a></li>
+    <li><a target='_blank' href="https://www.spotify.com/in-en/free/?utm_source=in-en_brand_contextual_text&utm_medium=paidsearch&utm_campaign=alwayson_asia_in_premiumbusiness_core_brand_neev+contextual+in-en+google&gclid=CjwKCAiAk--dBhABEiwAchIwkTxiqRy9SutEL530XWBUPNWiytTcxQuvErc7_yQSS4NErSGgzDd-nxoCPDUQAvD_BwE&gclsrc=aw.ds"><img src="https://img.icons8.com/fluency/2x/spotify.png" alt="" /></a></li>
+    <li><a target='_blank' href="https://music.youtube.com/"><img src="https://img.icons8.com/color/2x/youtube-music.png" alt="" /></a></li>
+    <li><a target='_blank' href="https://music.apple.com/us/browse"><img src="https://img.icons8.com/arcade/2x/apple-music.png" alt="" /></a></li>
+    
+</ul>
+
+    </>
+  )
+}
